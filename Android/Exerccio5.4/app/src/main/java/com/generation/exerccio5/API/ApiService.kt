f7bfc226c7c77d688781baa70ca6f75b84f0d231
Julia <julliaallves123@gmail.com>
2022-06-23 07:53:36 -0300

    Exerc novo

---
+++ Android/Exerccio5.4/app/src/main/java/com/generation/exerccio5/API/ApiService.kt
@@ -0,0 +1,24 @@
+package com.generation.exerccio5.API
+
+import com.generation.exerccio5.model.Categoria
+import com.generation.exerccio5.model.Tarefa
+import okhttp3.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiService {
+
+@GET ("categoria")
+suspend fun listCategoria(): retrofit2.Response<List<Categoria>>
+
+@POST("tarefa")
+suspend fun addTarefa(
+    @Body tarefa: Tarefa
+) : retrofit2.Response<Tarefa>
+
+@GET ("tarefa")
+suspend fun listTarefa(): retrofit2.Response<List<Tarefa>>
+
+
+}
